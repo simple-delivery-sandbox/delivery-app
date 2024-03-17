0eb8ee82d002b299e08bcd341981bcf667b9c922
Watson-Sei <55475145+Watson-Sei@users.noreply.github.com>
2024-03-17 21:23:48 +0900

    add production table

---
+++ db/migrations/000002_product_table.up.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS products (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    seller_id BIGINT UNSIGNED NOT NULL,
+    category VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (seller_id) REFERENCES users(id)
+) ENGINE=InnoDB;
